b6edffe6ba073c545fb0883a2c384cc938a13bee
LylaYuKakola <sherferyu@163.com>
2020-02-24 17:04:45 +0800

    feat(demo): 示例规范

--- demo/src/index.js
+++ demo/src/index.js
@@ -43,21 +43,25 @@ function IndexPage() {
     dispatch.markCurrentTimeAsync()
   }, [dispatch])
 
+  // 延迟两秒
   const handleClickGetTimeDelay = useCallback(() => {
     const dispatchWithDelay = dispatch.config({ delay: 2000 })
     dispatchWithDelay.markCurrentTimeAsync()
-  }, [])
+  }, [dispatch])
 
+  // 两秒防抖
   const handleClickGetTimeDebounce = useCallback(() => {
     const dispatchWithDebounce = dispatch.config({ debounce: 2000 })
     dispatchWithDebounce.markCurrentTimeAsync()
   }, [dispatch])
 
+  // 两秒节流
   const handleClickGetTimeThrottle = useCallback(() => {
     const dispatchWithThrottle = dispatch.config({ throttle: 2000 })
     dispatchWithThrottle.markCurrentTimeAsync()
   }, [dispatch])
 
+  // 设置挂起操作（可调大getTimeAsync中timer的时间）
   const handleClickGetTimePend = useCallback(() => {
     const dispatchWithPend = dispatch.config({ pend: true })
     dispatchWithPend.markCurrentTimeAsync()
@@ -65,38 +69,61 @@ function IndexPage() {
 
   const handleInsertWithParam = useCallback(() => {
     dispatch.insertWithParams(inputValue)
-  }, [inputValue])
+  }, [dispatch, inputValue])
 
   const handleChangeInputValue = useCallback(event => {
     setInputValue(event.target.value)
-  })
+  }, [])
 
   return (
     <div>
       <button onClick={handleInsertAsLast}>insert as the last one</button>
-      <button onClick={handleInsertTwiceAsLast}>insert as the last one (twice)</button><br />
+      <button onClick={handleInsertTwiceAsLast}>
+        insert as the last one (twice)
+      </button>
+      <br />
       <button onClick={handleInsertAsFirst}>insert as the first one</button>
-      <button onClick={handleInsertTwiceAsFirst}>insert as the first one (twice)</button><br />
+      <button onClick={handleInsertTwiceAsFirst}>
+        insert as the first one (twice)
+      </button>
+      <br />
       <button onClick={handleDeleteFirst}>delete the first one</button>
-      <button onClick={handleDeleteLast}>delete the last one</button><br />
-      <button onClick={handleClear}>clear</button><br />
+      <button onClick={handleDeleteLast}>delete the last one</button>
+      <br />
+      <button onClick={handleClear}>clear</button>
+      <br />
+      <br />
       <button onClick={handleClickGetTime}>getTime（Async）</button>
+      <br />
       <button onClick={handleClickGetTimeDelay}>getTime_Delay（Async）</button>
-      <button onClick={handleClickGetTimeDebounce}>getTime_Debounce（Async）</button>
-      <button onClick={handleClickGetTimeThrottle}>getTime_Throttle（Async）</button>
-      <button onClick={handleClickGetTimePend}>getTime_Pend（Async）</button><br />
+      <br />
+      <button onClick={handleClickGetTimeDebounce}>
+        getTime_Debounce（Async）
+      </button>
+      <button onClick={handleClickGetTimeThrottle}>
+        getTime_Throttle（Async）
+      </button>
+      <button onClick={handleClickGetTimePend}>getTime_Pend（Async）</button>
+      <br />
+      <br />
       <input type="text" value={inputValue} onChange={handleChangeInputValue} />
-      <button onClick={handleInsertWithParam}>insert text</button><br />
+      <button onClick={handleInsertWithParam}>insert text</button>
+      <br />
       <p>times: {timesInStore}</p>
       <p>async: {asyncInStore}</p>
       <p>sync: {syncInStore}</p>
       <br />
-      <ol style={{ width: 300, height: 300, overflow: 'auto', border: '1px solid red' }}>
-        {
-          dateListInStore.map(date => (
-            <li>{date}</li>
-          ))
-        }
+      <ol
+        style={{
+          width: 300,
+          height: 300,
+          overflow: 'auto',
+          border: '1px solid red',
+        }}
+      >
+        {dateListInStore.map((date, index) => (
+          <li key={`${date}-${index}`}>{date}</li>
+        ))}
       </ol>
     </div>
   )
